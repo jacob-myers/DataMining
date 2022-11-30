462026d12e5d411068117f1eb6f73d6f55ac8670
jacob-myers <jakemyers190@gmail.com>
2022-11-29 19:56:37 -0500

    Improving data cleaning. 	Rename ZIPcode column to ZIP 	Dropping blank rows 	Dropping ZIP = 99999 (catch-all zip) 	Dropping ZIP = 03291 (IRS stopped tracking after 2015)

--- NH_Trends/main.py
+++ NH_Trends/main.py
@@ -1,5 +1,11 @@
+# Data Source: https://www.irs.gov/statistics/soi-tax-stats-individual-income-tax-statistics-zip-code-data-soi
+# Author: Jake Myers
+
 import pandas as pd
 import numpy as np
+
+# Does a value exist in a column?: https://www.statology.org/pandas-check-if-value-in-column/
+
 
 def import_and_clean_irs(filename: str):
     # CLEAN COLUMNS
@@ -19,22 +25,71 @@ def import_and_clean_irs(filename: str):
     # https://stackoverflow.com/questions/61653697/pandas-str-replace-with-regex
     df.columns = df.columns.str.replace(r' \| Unnamed.*', '', regex=True)
     df.columns = df.columns.str.replace(r' \[\d*\]', '', regex=True)
-    df.columns = df.columns.str.replace(r'\\n', '', regex=True)
+    df.columns = df.columns.str.replace(r'\n', '', regex=True)
 
     # CLEAN DATA
     # TO DO
+    # Rename columns
+    df.rename(columns={'ZIPcode':'ZIP'}, inplace=True)
+
+    # Remove blank lines (Where ZIP code is blank) and end lines (dumb info)
+    # Forcing it to numeric, and deleting all non numeric, so Na and NaN
+    # https://stackoverflow.com/questions/42125131/delete-row-based-on-nulls-in-certain-columns-pandas
+    df['ZIP'] = pd.to_numeric(df['ZIP'], errors='coerce')
+    df.dropna(axis=0, subset=['ZIP'], inplace=True)
+
+    # Catch all ZIP code, not real.
+    # https://www.transportationinsight.com/resources/where-is-zip-code-99999-a-piece-of-clean-data-makes-a-big-difference/
+    indices = df[df['ZIP'] == 99999.0].index
+    df.drop(indices, inplace=True)
+    # IRS stopped recording this ZIP after 2015. Too small maybe? ~100 returns in 2015.
+    indices = df[df['ZIP'] == 03291.0].index
+    df.drop(indices, inplace=True)
+
+    # Reset index and delete old index.
+    df.reset_index(inplace=True)
+    del df['index']
 
     return df
 
 
 if __name__ == '__main__':
-    df_2019 = import_and_clean_irs("nh_2019.xlsx")
     df_2015 = import_and_clean_irs("nh_2015.xlsx")
+    #df_2016 = import_and_clean_irs("nh_2016.xlsx")
+    #df_2017 = import_and_clean_irs("nh_2017.xlsx")
+    #df_2018 = import_and_clean_irs("nh_2018.xlsx")
+    #df_2019 = import_and_clean_irs("nh_2019.xlsx")
+
     #Income tax before credit (2019) = Income tax (2015)
 
     cols_2015 = np.array(df_2015.columns)
-    #print(cols_2015)
 
+    # Same amount of empty rows?
+    """
+    print(df_2015.isnull().sum().sum())
+    print(df_2016.isnull().sum().sum())
+    print(df_2017.isnull().sum().sum())
+    print(df_2018.isnull().sum().sum())
+    print(df_2019.isnull().sum().sum())
+    """
+
+    # Identify ZIP codes that exist in 2015 and not 2016.
+    # https://stackoverflow.com/questions/26518802/pandas-iterating-through-each-row
+    """
+    rows = []
+    for row in df_2016.iterrows():
+        if pd.isna(row[1]['Size of adjusted gross income']):
+            rows.append(row[1]['ZIP'])
+
+    for row in df_2015.iterrows():
+        if pd.isna(row[1]['Size of adjusted gross income']):
+            if row[1]['ZIP'] not in rows:
+                print(row[1]['ZIP'])
+    """
+
+    # Columns that exist every year.
+    """
     for col in df_2015.columns:
-        if col not in df_2019.columns:
+        if col in df_2019.columns and col in df_2018.columns and col in df_2017 and col in df_2016:
             print(col)
+    """
