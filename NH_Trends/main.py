682b6d87f84218c3a5114a8d18bc35b7a48cee32
jacob-myers <jakemyers190@gmail.com>
2022-12-03 19:29:02 -0500

    Adding 2014 data (for making change-since-previous-year features) Adding function to create forest models. Using PCA to identify important features, using those features to make models. Slight bit of further data cleaning.

--- NH_Trends/main.py
+++ NH_Trends/main.py
@@ -1,8 +1,17 @@
 # Data Source: https://www.irs.gov/statistics/soi-tax-stats-individual-income-tax-statistics-zip-code-data-soi
 # Author: Jake Myers
 
+"""
+Testing 2015:
+Average score with 15 important features: 0.2816386355546581
+Average score with full dataset: 0.2903930567306121
+"""
+
 import pandas as pd
 import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
 
 # Does a value exist in a column?: https://www.statology.org/pandas-check-if-value-in-column/
 # Check null stuff: https://datatofish.com/check-nan-pandas-dataframe/
@@ -10,7 +19,7 @@ import numpy as np
 
 # Reads an IRS .xlsx file for a year in NH.
 # Returns a dataframe, cleaned up data by ZIP.
-def import_and_clean_irs(filename: str):
+def import_and_clean_irs(filename: str) -> pd.DataFrame:
     # CLEAN COLUMNS
     # Reading in data sheet with a multi-level column index.
     # https://stackoverflow.com/questions/69855943/pandas-read-csv-with-columns-name-spread-over-multiple-rows
@@ -34,6 +43,8 @@ def import_and_clean_irs(filename: str):
     # TO DO
     # Rename columns
     df.rename(columns={'ZIPcode':'ZIP'}, inplace=True)
+    df.rename(columns={'Number of dependent exemptions': 'Number of dependents'}, inplace=True)  # 2017
+
 
     # Remove blank lines (Where ZIP code is blank) and end lines (dumb info)
     # Forcing it to numeric, and deleting all non numeric, so Na and NaN
@@ -62,8 +73,8 @@ def import_and_clean_irs(filename: str):
 
 # Takes a dataframe, the tax data by zip/size of gross income.
 # Returns a dataframe, the tax data by zip, with each size as a feature.
-def reshape_by_ZIP(df: pd.DataFrame):
-    
+def reshape_by_ZIP(df: pd.DataFrame) -> pd.DataFrame:
+
     # Dataframe with just totals for each zip.
     df_totals = df.loc[df['Size of adjusted gross income'].isnull()]
     df_totals = df_totals.set_index('ZIP')
@@ -120,13 +131,88 @@ def reshape_by_ZIP(df: pd.DataFrame):
     return df_combined
 
 
+# Takes two dataframes, the year being looked at, and the previous year.
+# Creates the change-in features, adds them to the current year df, and returns it.
+def create_deltas(cur_df: pd.DataFrame, prev_df: pd.DataFrame) -> pd.DataFrame:
+    cur_df['Change in returns since previous year'] = cur_df['Number of returns'] - prev_df['Number of returns']
+    # Dependents count not present in 2019
+    if 'Number of dependents' in cur_df.columns:
+        cur_df['Change in dependents since previous year'] = cur_df['Number of dependents'] - prev_df['Number of dependents']
+    return cur_df
+
+
+# Takes a dataframe (of a year) and builds a RandomForestRegressor from it.
+# returns the score and model.
+def create_forest(df: pd.DataFrame) -> (float, object):
+    # Target: Change in returns.
+    # Features: Everything else.
+    X_train, X_test, y_train, y_test = train_test_split(
+        df.drop(columns=['Change in returns since previous year']),
+        df['Change in returns since previous year'],
+        test_size=.25,
+        random_state=42)
+    rf = RandomForestRegressor(max_depth=5)
+    rf_reg = rf.fit(X_train, y_train)
+    return rf.score(X_test, y_test), rf_reg
+
+
+# Use PCA to get the n most important columns. Returns new dataframe.
+# Reference: https://stackoverflow.com/questions/50796024/feature-variable-importance-after-a-pca-analysis
+def get_f_important_features(X, f: int, n_components: float):
+    pca = PCA(n_components=n_components)
+    pca_model = pca.fit_transform(X)
+
+    # Number of components
+    n_pcs = pca.components_.shape[0]
+    # Index of most important feature in each component
+    most_important = [np.abs(pca.components_[i]).argmax() for i in range(n_pcs)]
+    # Feature names
+    initial_feature_names = [col for col in X.columns]
+    # Putting feature names in order of importance
+    most_important_names = [initial_feature_names[most_important[i]] for i in range(n_pcs)]
+
+    # Uses the first f column names (most important) to build a new dataframe.
+    return X[most_important_names[:f]]
+
+
+"""
+# Engineer features.
+def engineer_tax_df(df: list[pd.DataFrame]) -> pd.DataFrame:
+    pass
+"""
+
+
+# Runs all cleaning, reshaping, engineering, scoring on a year (needs prev year as well)
+def test_year(year: int) -> None:
+    raw_df = import_and_clean_irs(f"nh_{year}.xlsx")
+    raw_prev_df = import_and_clean_irs(f"nh_{year - 1}.xlsx")
+
+    df = reshape_by_ZIP(raw_df)
+    df_prev = reshape_by_ZIP(raw_prev_df)
+
+    df = create_deltas(df, df_prev)
+
+    #for i in range(10):
+    #    rf_score, rf_reg = create_forest(df)
+    #    print(rf_score)
+
+    avg_score = 0
+    for i in range(100):
+        rf_score, rf_reg = create_forest(df)
+        avg_score += rf_score
+    avg_score /= 100
+    print(f'Average score with full dataset: {avg_score}')
+
+
 if __name__ == '__main__':
+    raw_2014 = import_and_clean_irs("nh_2014.xlsx")
     raw_2015 = import_and_clean_irs("nh_2015.xlsx")
     #raw_2016 = import_and_clean_irs("nh_2016.xlsx")
     #raw_2017 = import_and_clean_irs("nh_2017.xlsx")
     #raw_2018 = import_and_clean_irs("nh_2018.xlsx")
     #raw_2019 = import_and_clean_irs("nh_2019.xlsx")
 
+    df_2014 = reshape_by_ZIP(raw_2014)
     df_2015 = reshape_by_ZIP(raw_2015)
     #df_2016 = reshape_by_ZIP(raw_2016)
     #df_2017 = reshape_by_ZIP(raw_2017)
@@ -136,9 +222,45 @@ if __name__ == '__main__':
     # Income tax before credit (2019) = Income tax (2015)
     # Note: cols_2015 = np.array(raw_2015.columns)
 
-    print(df_2015.info(verbose=True))
+    #print(df_2015.info(verbose=True))
+
+    #test_year(2019)
 
 
+    """
+    X_train, X_test, y_train, y_test = train_test_split(
+        df_2015.drop(columns=['Change in returns since previous year']),
+        df_2015['Change in returns since previous year'],
+        test_size=.25,
+        random_state=42)
+    """
+
+    df_2015 = create_deltas(df_2015, df_2014)
+    important = get_f_important_features(df_2015.drop(columns=['Change in returns since previous year']), 15, 15)
+    important['Change in returns since previous year'] = df_2015['Change in returns since previous year']
+    #print(important.sample(10))
+
+    avg_score_small_set = 0
+    for i in range(100):
+        rf_score, rf_reg = create_forest(important)
+        avg_score_small_set += rf_score
+    avg_score_small_set /= 100
+    print(f'Average score with 15 important features: {avg_score_small_set}')
+
+    test_year(2015)
+
+    """
+    pca = PCA(n_components=10)
+    X = df_2015.drop(columns=['Change in returns since previous year'])
+    pca_model = pca.fit_transform(X)
+    n_pcs = pca.components_.shape[0]
+    print(n_pcs)
+    most_important = [np.abs(pca.components_[i]).argmax() for i in range(n_pcs)]
+    initial_feature_names = [col for col in X.columns]
+    most_important_names = [initial_feature_names[most_important[i]] for i in range(n_pcs)]
+    for i in most_important_names:
+        print(i)
+    """
 
     """
     # All are 229, meaning one null (Size of adjusted gross income) per ZIP
